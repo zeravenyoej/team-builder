9f54a7cb836e2ca06a4a5ac4422630fb69e4162d
Joey Nevarez <josephnevarez12@gmail.com>
2019-11-04 14:25:33 -0800

    data is rendering on screen officially. will add styling later

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Data from './Data';
+import TeamCard from './Components/TeamCard';
 import Form from './Components/Form';
+
 
 function App() {
 
   const [teamMembers, setTeamMembers] = useState(Data);
   console.log(teamMembers);
-  
+
   return (
     <div className="App">
-      <Form/>
+
+      <Route path='/' render={(props)=>
+        <TeamCard 
+          teamMembers={teamMembers}
+          /> 
+      }/>
+
+      <Route path='/' render={(props)=>
+        <Form
+        setTeamMembers={setTeamMembers}
+        />
+      }/>
     </div>
   );
 }
